8fa01521f8d89895dca77eeffa782818dc4c0d20
Aditi <aditiadi39@gmail.com>
2020-04-15 03:56:57 -1200

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates/views')
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server started listening at 3000')
+app.listen(port, () => {
+    console.log('server started listening at '+port)
 })
